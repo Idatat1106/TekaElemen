a966a7cbae33309b30355cad8acc6f9dbe4029e6
Idatat1106 <106068466+Idatat1106@users.noreply.github.com>
2022-05-24 17:58:20 +0800

    Update Game.py
    Finalize

--- Game.py
+++ Game.py
@@ -4,37 +4,64 @@ import random
 
 def main():
     
-    #Prepare the element list
-    ElemenLists=["Hydrogen","Helium","Lithium","Carbon","Nitrogen","Oxygen","Fluorine","Neon","Aluminium","Silicon","Sulfur","Chlorine","Krypton"]
-    clue1=["Water Constituem","Balloon","Battery","The most organic compount","Most needed by plant","Most needed by human",
-            "toothpaste","To protect bulb elemen from burn","Conduct heat efficiently","Glass","firecrackers","we drink in tap water","feared superman"]
-
-       
+    #Prepare the elements ad clues list
+    ElemenLists=[
+        "Hydrogen",
+        "Helium",
+        "Lithium",
+        "Carbon",
+        "Nitrogen",
+        "Oxygen",
+        "Fluorine",
+        "Neon",
+        "Aluminium",
+        "Silicon",
+        "Sulfur",
+        "Chlorine",
+        "Krypton"
+    ]
+    clue1=[
+        "Water Constituem",
+        "Balloon",
+        "Battery",
+        "The most organic compount",
+        "Most needed by plant",
+        "Most needed by human",
+        "toothpaste",
+        "To protect bulb elemen from burn",
+        "Conduct heat efficiently",
+        "Glass",
+        "firecrackers",
+        "we drink in tap water",
+        "feared superman"
+    ]
+     
     
     #The game start with 3 chances to guess the element
     while True:
 
         #Initialise the variables
         Ind=random.randint(0,12)
-        guess=None
+        guess="None"
         count=3
-        while ElemenLists[Ind]!=guess and count>0:
+
+        while ElemenLists[Ind].lower()!=guess.lower() and count>0:
             guess=input("Guess what is the name of periodic element I'm thinking of? ")
-            if ElemenLists[Ind]==guess:
+            if ElemenLists[Ind].lower()==guess.lower():
                 if count==3:
                     print("Do you read my mind???....Bravo you are superb")
                 else:
-                    print("You genius!")
+                    print("You are genius!")
             else:
                 count-=1 #Reduce the chances
                 print("Try again, you have ",count," Chance(s)\n")
                 print("Here some clues: ",clue1[Ind])
-            
         if count<=0:
             cont=input("Oh no...Want to try again? Y/N ")
         else:
             cont=input("Guess more? Y/N ")
-        if cont=="N":
+        if cont.capitalize()=="N":
+            print("THANK FOR TESTING MY GAME")
             break 
-
+    
 main()
